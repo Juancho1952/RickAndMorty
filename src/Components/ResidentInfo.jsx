2acbfed8c16b17eba904e3dd0fa7930198b0bf55
juancho1952 <juancho1952@gmail.com>
2022-10-06 18:31:04 -0500

    Cambiando Informacion Plantilla

--- src/Components/ResidentInfo.jsx
+++ src/Components/ResidentInfo.jsx
@@ -11,7 +11,7 @@ const ResidentInfo = ({ url }) => {
             <img className='card-img' src={resident?.image} alt="Photo character" />
             <h2 className='card-tittle'><span></span>{resident?.name}</h2>
             <ul className='card-info'>
-                <li className='card-data'><span>Raza</span>{resident?.status}</li>
+                <li className='card-data'><span>Estado</span>{resident?.status}</li>
                 <li className='card-data'><span>Origen</span>{resident?.species}</li>
                 <li className='card-data'><span>Lugar de origen</span>{resident?.origin.name}</li>
                 <li className='card-data'><span>Aparicion en Episodios</span>{resident?.episode.length}</li>
